54bc9b1087553f511e5f942fe84c8585a2d840cc
User-XYZ <nwosu4686@gmail.com>
2024-02-10 21:08:59 +0100

    updated ClickToAction section

--- src/components/ClickToAction.jsx
+++ src/components/ClickToAction.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import styles from '../style';
+import Button from './Button';
 
-const ClickToAction = () => {
-  return (
+const ClickToAction = () => (
+  <section>
     <div>
-      CTA
+      <h2>Let's try our service now!</h2>
+      <p>Everything you need to accept card payments and grow your business anywhere on the palnet.</p>
     </div>
-  )
-}
+
+    <div>
+      <Button />
+    </div>
+  </section>
+)
 
 export default ClickToAction
